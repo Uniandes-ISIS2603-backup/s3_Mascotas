4435a0711dd8c06d00c691c122bb2229779f2d27
estudiante <estudiante@ISIS2603-297.sis.virtual.uniandes.edu.co>
2018-08-29 17:37:13 -0500

    Reparacion

--- s3_mascotas-api/src/main/java/co/edu/uniandes/csw/mascotas/dtos/CompraDTO.java
+++ s3_mascotas-api/src/main/java/co/edu/uniandes/csw/mascotas/dtos/CompraDTO.java
@@ -7,35 +7,8 @@ package co.edu.uniandes.csw.mascotas.dtos;
 
 /**
  *
- * @author Juan Sebastián Gómez
+ * @author estudiante
  */
 public class CompraDTO {
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    public String getTipoDePago() {
-        return tipoDePago;
-    }
-
-    public void setTipoDePago(String tipoDePago) {
-        this.tipoDePago = tipoDePago;
-    }
-
-    public CompraDTO(double precio, String tipoDePago) {
-        this.precio = precio;
-        this.tipoDePago = tipoDePago;
-    }
-    /**
-     * Atributo que representa el precio de la mascota
-     */
-        private double precio;
-        private String tipoDePago;
-        
-        
+    
 }
